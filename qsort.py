5ac5c7b6502f34d32bc1ed1a078b51a180746fd1
Eduardo Costa de Souza <104004842+edu15076@users.noreply.github.com>
2022-10-09 14:24:14 -0300

    Quick sort

---
+++ qsort.py
@@ -0,0 +1,22 @@
+from inspect import _void
+import random
+
+def quick_sort(li:list, start = 0, end = None):
+    def partition(li, start, end):
+        count = start
+        pivot_index = random.randint(start, end)
+        li[pivot_index], li[end] = li[end], li[pivot_index]
+        piv = li[end]
+        for i in range(start, end):
+            if li[i] <= piv:
+                li[i], li[count] = li[count], li[i]
+                count += 1
+        li[end], li[count] = li[count], li[end]
+        return count
+    
+    if end == None: end = len(li) - 1
+
+    if start < end:
+        ciner_index = partition(li, start, end)
+        quick_sort(li, start, ciner_index - 1)
+        quick_sort(li, ciner_index + 1, end)
